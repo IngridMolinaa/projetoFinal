59cbfc66ce14e4666955b6b30aed29c8cb09586f
Ingrid Molina <ingridmolinahh@gmail.com>
2023-08-16 15:10:54 -0300

    criar login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponentComponent } from './components/page-not-found-component/page-not-found-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login',        component: LoginComponent },
+  { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponentComponent } from './components/page-not-found-component/page-not-found-component.component';
+import { Pagina2Component } from './component/pagina2/pagina2.component';
+import { AvaliacaoComponent } from './components/avaliacao/avaliacao.component';
+import { PesquisaComponent } from './components/pesquisa/pesquisa.component';
+import { ProcessoComponent } from './components/processo/processo.component';
+import { FazerComponent } from './components/fazer/fazer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PageNotFoundComponentComponent,
+    Pagina2Component,
+    AvaliacaoComponent,
+    PesquisaComponent,
+    ProcessoComponent,
+    FazerComponent
   ],
   imports: [
     BrowserModule,
